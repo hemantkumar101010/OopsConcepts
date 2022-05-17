d2f839bc710c2c0c47c531c99d96dc903f689234
hemantkumar101010 <heman5502@gmail.com>
2022-05-17 17:48:20 +0530

    Add project files.

---
+++ OppsConcepts/Mobile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    internal class Mobile
+    {
+        string modelName = "iPhone";
+        int price = 12000;
+        public void displayModel()
+        {
+            Console.WriteLine("Your model name is "+ modelName +" & price is "+price);
+
+        }
+    }
+}
---
+++ OppsConcepts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobile mobObject1 = new Mobile();
+            mobObject1.displayModel();
+        }
+    }
+}
